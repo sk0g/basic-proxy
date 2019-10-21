d7dccaeff0c774b841c0f2e56976edfdd59ec084
sk0g <ani.mehta@mail.com>
2019-10-21 15:26:48 +1000

    Handle GET requests

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"strconv"
@@ -10,6 +11,10 @@ const port = 8899
 
 func main() {
 	r := gin.Default()
+
+	r.Handle("GET", "/proxyGET", func(c *gin.Context) {
+		fmt.Println("GET request detected")
+	})
 
 	log.Fatal(r.Run(":" + strconv.Itoa(port)))
 }
