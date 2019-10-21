8237d5ddaa087ca2e767c5652caa230caa32264e
sk0g <ani.mehta@mail.com>
2019-10-21 15:28:13 +1000

    Detect GET and POST, more elegantly

--- main.go
+++ main.go
@@ -12,8 +12,12 @@ const port = 8899
 func main() {
 	r := gin.Default()
 
-	r.Handle("GET", "/proxyGET", func(c *gin.Context) {
+	r.GET("/proxyGET", func(c *gin.Context) {
 		fmt.Println("GET request detected")
+	})
+
+	r.POST("/proxyPOST", func(c *gin.Context) {
+		fmt.Println("POST request detected")
 	})
 
 	log.Fatal(r.Run(":" + strconv.Itoa(port)))
