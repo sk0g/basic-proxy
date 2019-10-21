85252551a3fe28a3d99e6f7cae6e03861ff1bc3c
sk0g <ani.mehta@mail.com>
2019-10-21 15:17:54 +1000

    Update routing, package name

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sk0g/basic-proxy
 
 go 1.12
+
+require github.com/gin-gonic/gin v1.4.0
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-package basic_proxy
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"strconv"
+)
+
+const port = 8899
+
+func main() {
+	r := gin.Default()
+
+	log.Fatal(r.Run(":" + strconv.Itoa(port)))
+}
